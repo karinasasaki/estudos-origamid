a59932c6f819cdd3d66d4fffa0a7078ed0d73de2
Karina Sasaki <karinasasaki99@gmail.com>
2023-08-14 18:57:36 -0400

    Aula 0202

--- tipos-de-dados/script.js
+++ tipos-de-dados/script.js
@@ -10,3 +10,8 @@ var gols = 1000;
 var frase = "Romário fez " + gols + " gols";
 console.log(frase, typeof frase)
 
+var melhor = "pior"
+var frase2 = "Esse é o \"melhor\" jogo";
+var frase3 = "Esse é o 'melhor' jogo";
+var frase4 = `Esse é o ${melhor} jogo`;
+console.log(frase2, frase3, frase4);
