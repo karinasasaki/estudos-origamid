a9ce97d810a2e083452f334a6b23270727f28d6c
karinasasaki <karinasasaki@github.com>
2023-08-14 13:00:29 -0400

    Aula 0202 10:50

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,12 @@
+var nome = "Karina";
+var idade = 23;
+console.log(typeof nome, typeof idade);
+
+var sobrenome = "Sasaki";
+nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = "Romário fez " + gols + " gols";
+console.log(frase, typeof frase)
+
