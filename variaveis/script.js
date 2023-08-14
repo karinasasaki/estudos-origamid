46db87b6b4f87b4b29bfa64588400604e96765f3
karinasasaki <karinasasaki@github.com>
2023-08-14 12:39:11 -0400

    Aula 0201

---
+++ variaveis/script.js
@@ -0,0 +1,18 @@
+//Variáveis
+var nome = "Karina";
+var idade = 23;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+//Outra forma de declarar variáveis
+var preco = 25,
+    totalComprado = 5,
+    totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+//Variável declarada sem valor, retorna undefined
+var semValor;
+
+console.log(semValor)
