320576e4f20c9715c0e4773664f481f0db0d6522
Karina Sasaki <karinasasaki99@gmail.com>
2023-09-04 19:35:27 -0400

    Aula 0302

--- numeros-e-operadores/script.js
+++
@@ -1,7 +0,0 @@
-//Operadores aritméticos com strings
-var soma = "100" + 50; //concatena
-var subtracao = "100" - 50;
-var multiplicacao = "100" * "2"
-var divisao = "Comprei 10" / 2; //NaN
-console.log(soma, subtracao, multiplicacao, divisao);
-console.log(isNaN(divisao));
--- tudo-e-objeto/script.js
+++
@@ -1,4 +0,0 @@
-var btn = document.querySelector(".btn");
-btn.addEventListener("click", function() {
-  console.log("Clicou");
-})
