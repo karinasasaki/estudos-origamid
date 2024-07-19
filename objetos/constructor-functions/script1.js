76dbf63950e52993b6970879d473ff8e3ebc13bd
Karina Sasaki <karinasasaki99@gmail.com>
2024-07-19 16:07:39 -0400

    Aula 0401

---
+++ objetos/constructor-functions/script1.js
@@ -0,0 +1,10 @@
+/* function Carro(marca, precoInicial) {
+  const taxa = 1.2;
+  const precoFinal = precoInicial * taxa;
+
+  this.marca = marca;
+  this.preco = precoFinal;
+}
+
+const honda = new Carro("Honda", 3000);
+const fiat = new Carro("Fiat", 2000); */
---
+++ objetos/constructor-functions/script2.js
@@ -0,0 +1,11 @@
+/* function Dom(seletor) {
+  this.element = function() {
+    return document.querySelector(seletor);
+  }
+  this.ativar = function() {
+    this.element().classList.add("ativar");
+  }
+}
+
+const li = new Dom("li");
+li.ativar(); */
